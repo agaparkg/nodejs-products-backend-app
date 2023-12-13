961bb2784abf46bb54e0902f736390d685249514
Azamat Gaparov <agaparovkg@gmail.com>
2023-12-12 21:33:54 -0800

    Updated app.js

--- app.js
+++ app.js
@@ -159,5 +159,5 @@ app.patch("/products/:id", async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
